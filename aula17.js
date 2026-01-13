274a5a368dd78e8243cad54b957664ae8d429731
emirfadul <emirfadul@emirfadul3.local>
2026-01-13 12:24:37 -0400

    atualizado em 13.jan as 12h24

--- aula17.js
+++ aula17.js
@@ -4,17 +4,35 @@ let entrada = ""; // simular trocando o valor para 1 e 2
 
 
 function reproduzirFilme(){
-  console.log("Tecla PLAY acionada.")
+  console.log("Tecla PLAY acionada.");
 }
 
 function pausarFilme(){
-  console.log("Tecla PAUSE acionada.")
+  console.log("Tecla PAUSE acionada.");
 }
 
 function sairFilme(){
-  console.log("Sair do Filme.")
+  console.log("Sair do Filme.");
 }
 
 function opcoesConfiguracao(entrada){
-  
+
+  do{
+    console.log("Opçoes: 1 - pausar, 2 - sair");
+    console.log("opçáo: "+entrada);
+
+    if(entrada === 1){
+      pausarFilme();
+      reproduzido = false;
+    }else if(entrada === 2){
+      sairFilme();
+      reproduzido = false;
+    }else{
+      console.log("Opçáo inv[alida.");
+      break
+    }
+  }while(reproduzindo);  
 }
+
+reproduzirFilme();
+opcoesConfiguracao(entrada);
