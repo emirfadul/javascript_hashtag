1ce37d7e8a2afa80eedfedcf12b2cc8272290910
emirfadul <emirfadul@emirfadul3.local>
2026-01-15 12:49:44 -0400

    atualizado em 15jan as 12h49

---
+++ exercicio.js
@@ -0,0 +1,45 @@
+// exercicio 1
+// let compra;
+// function verificaDesconto(compra){   
+//     if (compra >= 100){
+//         let desconto = compra * 0.1;
+//         console.log("Voce tem direito a desconto no valor de R$ "+desconto);
+//     }else{
+//         console.log("Nenhum desconto aplicado.")
+//     }
+// }
+
+// verificaDesconto(120);
+
+// ============================================================================
+
+// exercicio 2
+
+// let produto = 5;
+
+// function verificaEstoque(){
+//  if(produto > 0){
+//     return console.log("Produto Disponivel.");
+//  }else {
+//     return console.log("Produto indisponível.")
+//  }
+// }
+
+// verificaEstoque();
+
+// =================================================================
+
+// exercicio 3
+let pontos = 0;
+
+function categoriaCliente(pontos){  
+    if (pontos >= 85){
+        return console.log("Clinte Premium.")
+    }else if (pontos <= 84 && pontos >= 50){
+        return console.log("Cliente Regular.")
+    }else{
+        return console.log("Cliente Comum.")
+    }
+}
+
+categoriaCliente(45);
